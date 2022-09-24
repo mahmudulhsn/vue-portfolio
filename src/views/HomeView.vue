3684533ee428b0f2e3da7ebd7b93b83bd3f9a133
parvez khan <mpkhasan62@gmail.com>
2022-09-24 23:30:34 +0600

    fix: heading change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@
     "
   >
     <div class="">
-      <h1 class="text-center text-xl mb-10">Hello</h1>
+      <h1 class="text-center text-xl mb-10">hi</h1>
       <h1 class="text-5xl">I Am A UI/UX Designer.</h1>
       <ul class="flex mt-10 justify-center skills">
         <li class="px-3 py-2">App Design</li>
